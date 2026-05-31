011a967bb4a67b41fbb658983933bf09c221a760
yoshimidevz <yoshimidev1@gmail.com>
2026-05-31 11:04:38 -0300

    feat:  gera categoria aleatória coerente com o tipo da transação gerada.

--- lib/helper/transaction_fake_factory.dart
+++ lib/helper/transaction_fake_factory.dart
@@ -1,5 +1,6 @@
-
+import 'dart:math';
 import 'package:faker_dart/faker_dart.dart';
+import '../domain/entity/transaction_category.dart';
 import '../domain/entity/transaction_entity.dart';
 
 abstract class TransactionFakeFactory {
@@ -7,23 +8,26 @@ abstract class TransactionFakeFactory {
     final faker = Faker.instance;
     faker.setLocale(FakerLocaleType.pt_PT);
 
-    var instance = TransactionEntity(
+    final type = faker.datatype.boolean()
+        ? TransactionType.income
+        : TransactionType.expense;
+
+    final categories = TransactionCategoryExtension.forType(type);
+    final category = categories[Random().nextInt(categories.length)];
+
+    return TransactionEntity(
       title: faker.commerce.productName(),
-      type:
-          (faker.datatype.boolean())
-              ? TransactionType.income
-              : TransactionType.expense,
+      type: type,
+      category: category,
       date: faker.date.between(
-        DateTime.now().subtract(Duration(days: 30)),
+        DateTime.now().subtract(const Duration(days: 30)),
         DateTime.now(),
       ),
       amount: faker.datatype.float(
         min: 100.0,
-        // min: 1.0 * math.pow(10.0, faker.datatype.number(min: 1, max: 3)),
         max: 2000.0,
         precision: 2,
       ),
     );
-    return instance;
   }
 }
